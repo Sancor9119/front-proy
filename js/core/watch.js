288cca2e2b0da32ed38dd001d48782955c27bd69
MurilloDiana <dianamurillo.467@gmail.com>
2023-10-17 11:00:51 -0400

    xd

--- js/core/watch.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const io = require('socket.io-client');
-const socket = io.connect('http://localhost:9090');
-console.log('socket cargado...');
-
-module.exports =  {
-    io,
-    socket,
-};
